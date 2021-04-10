4ee65e0be1f8fff2fe5bc3d1498f73df04721dae
Gavin Coulthurst <gcoulthurst18@gmail.com>
2021-04-10 11:19:16 -0400

    Added basic implementation of Grid class

---
+++ model/Grid.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.io.Serializable;
+
+public class Grid implements Serializable{
+	
+	// Used for saving Grid objects to a file, increase by 1 when adding / removing fields.
+	private static final long serialVersionUID = 1L;
+	
+	private GridCell[][] grid;
+	private int windDirection;
+	private int windSpeed;
+	
+	public Grid(int height, int width)
+	{
+		grid = new GridCell[height][width];
+		windDirection = 0;
+		windSpeed = 0;
+	}
+	
+	public Grid(int height, int width, int windDirection, int windSpeed)
+	{
+		grid = new GridCell[height][width];
+		this.windDirection = windDirection;
+		this.windSpeed = windSpeed;
+	}
+	
+	public GridCell getGridCell(int x, int y)
+	{
+		return grid[x][y];
+	}
+
+	public int getWindDirection() 
+	{
+		return windDirection;
+	}
+
+	public int getWindSpeed() 
+	{
+		return windSpeed;
+	}
+	
+	public void setGridCell(GridCell cell, int x, int y)
+	{
+		grid[x][y] = cell;
+	}
+	
+	public void setWindDirection(int windDirection) 
+	{
+		this.windDirection = windDirection;
+	}
+
+	public void setWindSpeed(int windSpeed) 
+	{
+		this.windSpeed = windSpeed;
+	}
+}

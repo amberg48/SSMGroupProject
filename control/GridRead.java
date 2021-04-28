3b8b1b0c2457d184366738faf9db96b192e73531
Mathew Marchiano <marchianm9@students.rowan.edu>
2021-04-28 19:35:50 -0400

    getSurroundings() method fully implemented. Tested

--- control/GridRead.java
+++ control/GridRead.java
@@ -1,5 +1,6 @@
 package control;
 import model.Grid;
+import model.GridCell;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,23 +26,25 @@ public class GridRead {
     }
 
     /**
-     * Returns all grids from a given x, y coordinate
+     * Returns all grid cells from a given x, y coordinate.
+     * WILL NOT include given coordinate in returned List.
+     * 
      * @param matrix The 2d array that the Grid is composed of.
      * @param x X coordinate
      * @param y Y coordinate
      * @return List of surrounding items.
      */
-    public static List<Integer> getSurroundings(int[][] matrix, int x, int y){
-      List<Integer> res = new ArrayList<Integer>();
-      for (int[] direction : directions) {
-          int cx = x + direction[0];
-          int cy = y + direction[1];
-          if(cy >=0 && cy < matrix.length)
-              if(cx >= 0 && cx < matrix[cy].length)
-                  res.add(matrix[cy][cx]);
-      }
-      
-      return res;
-}
+    public List<GridCell> getSurroundings(Grid grid, int x, int y){
+        List<GridCell> res = new ArrayList<GridCell>();
+        for (int[] direction : directions) {
+            int cx = x + direction[0];
+            int cy = y + direction[1];
+            if(cy >=0 && cy < grid.getGridCells().length)
+                if(cx >= 0 && cx < grid.getGridCells()[cy].length)
+                    res.add(grid.getGridCells()[cy][cx]);
+        }
+        
+        return res;
+    }
     
 }

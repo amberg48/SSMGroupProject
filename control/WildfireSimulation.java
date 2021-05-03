2fae0a0ddfc92f6788dd6161fe2020528f0e1304
Gavin Coulthurst <gcoulthurst18@gmail.com>
2021-05-03 10:08:53 -0400

    Updated driver to run simulation multiple times

--- control/WildfireSimulation.java
+++ control/WildfireSimulation.java
@@ -12,44 +12,60 @@ public class WildfireSimulation {
 	public static void main(String[] args) throws InterruptedException {
 		String input = "grids/random.ser";
 		
-		// Read grid from given file
-		Grid grid = null;
-		try 
-		{
-			FileInputStream fileIn = new FileInputStream(input);
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			grid = (Grid) in.readObject();
-			in.close();
-			fileIn.close();
-		} 
-		catch (IOException i) 
-		{
-			i.printStackTrace();
-		} 
-		catch (ClassNotFoundException c) 
-		{
-			System.out.println("Grid class not found");
-			c.printStackTrace();
-		}
+		int x = 10;
+		int y = 10;
+		int count = 0;
 		
-		System.out.println(grid);
-
-		GridDisplay display = new GridDisplay(400, 400, 20, 20);
-
-		// Create controller
-		GridRead controller = new GridRead(grid, display);
-		display.initDraw();
-		display.updateGrid(controller.gridToDisplay(grid));
-		
-		// Temporary, runs Simulation for 20 steps then ends.
-		for (int i = 0; i < 20; i++) 
+		for (int i = 0; i < 100; i++)
 		{
-			// Updates controller
-			controller.step();
-			// Update display
-			System.out.println(grid.toString());
+			
+			// Read grid from given file
+			Grid grid = null;
+			try 
+			{
+				FileInputStream fileIn = new FileInputStream(input);
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				grid = (Grid) in.readObject();
+				in.close();
+				fileIn.close();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			} 
+			catch (ClassNotFoundException c) 
+			{
+				System.out.println("Grid class not found");
+				c.printStackTrace();
+			}
+
+			GridDisplay display = new GridDisplay(400, 400, 20, 20);
+
+			// Create controller
+			GridRead controller = new GridRead(grid, display);
+			display.initDraw();
 			display.updateGrid(controller.gridToDisplay(grid));
-			Thread.sleep(500);
+		
+			boolean onFire = false;
+			for (int j = 0; j < 30; j++) 
+			{
+				// Updates controller
+				controller.step();
+				// Update display
+				display.updateGrid(controller.gridToDisplay(grid));
+				Thread.sleep(20);
+				if(grid.getGridCell(x, y).getOnFire())
+				{
+					onFire = true;
+				}
+			}
+			
+			if (onFire)
+			{
+				count++;
+				
+			}
+			System.out.println("Iteration: " + i + "\tCell On Fire: " + grid.getGridCell(x, y).getOnFire() + "\tCount: " + count + "\tP: " + count/(i+1.0));
 		}
 
 	}
